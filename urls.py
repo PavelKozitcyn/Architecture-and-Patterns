021adaaa107f2bdb9647c989e906da22f19b7f63
paul <kpasha11@mail.ru>
2023-05-08 14:41:33 +0300

    lesson_4

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from datetime import date
-from views import Index, Contact, Another, Examples, Page, MessageView
+from views import Index, About, StudyPrograms, CoursesList, \
+    CreateCourse, CreateCategory, CategoryList, CopyCourse
 
 
 # front controller
@@ -15,9 +16,11 @@ fronts = [secret_front, other_front]
 
 routes = {
     '/': Index(),
-    '/contact/': Contact(),
-    '/another/': Another(),
-    '/examples/': Examples(),
-    '/page/': Page(),
-    '/message/': MessageView(),
+    '/about/': About(),
+    '/study_programs/': StudyPrograms(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList(),
+    '/copy-course/': CopyCourse()
 }
--- views.py
+++ views.py
@@ -1,55 +1,139 @@
+from datetime import date
+
+from patterns.сreational_patterns import Engine, Logger
 from design_framework.templator import render
-#import datetime
-#import os
+site = Engine()
+logger = Logger('main')
 
 
+# контроллер - главная страница
 class Index:
     def __call__(self, request):
-        return '200 OK', render('index.html', date=request.get('date', None))
+        return '200 OK', render('index.html', objects_list=site.categories)
 
 
-class Contact:
+# контроллер "О проекте"
+class About:
     def __call__(self, request):
-        return '200 OK', render('contact.html', date=request.get('date', None))
+        return '200 OK', render('about.html')
 
 
-class Examples:
+# контроллер - Расписания
+class StudyPrograms:
     def __call__(self, request):
-        return '200 OK', render('examples.html', date=request.get('date', None))
+        return '200 OK', render('study-programs.html', date=date.today())
 
 
-class Page:
+# контроллер 404
+class NotFound404:
     def __call__(self, request):
-        return '200 OK', render('page.html', date=request.get('date', None))
+        return '404 WHAT', '404 PAGE Not Found'
 
 
-class Another:
+# контроллер - список курсов
+class CoursesList:
     def __call__(self, request):
-        return '200 OK', render('another.html', date=request.get('date', None))
+        logger.log('Список курсов')
+        try:
+            category = site.find_category_by_id(
+                int(request['request_params']['id']))
+            return '200 OK', render('course_list.html',
+                                    objects_list=category.courses,
+                                    name=category.name, id=category.id)
+        except KeyError:
+            return '200 OK', 'No courses have been added yet'
 
 
-class MessageView:
+# контроллер - создать курс
+class CreateCourse:
+    category_id = -1
+
     def __call__(self, request):
-        return '200 OK', render('contact_lesson2.html', date=request.get('date', None))
-    # def __call__(self, request):
-    #     # Проверка метода запроса
-    #     if request['method'] == 'POST':
-    #         now = datetime.datetime.now()
-    #         data = request['data']
-    #         title = data['title']
-    #         text = data['text']
-    #         email = data['email']
-    #         if not os.path.exists('messages'):
-    #             os.mkdir('messages')
-    #         # print(f'Нам пришло сообщение от {email} с темой {title} и текстом {text}')
-    #         with open(f'messages/message_{now.strftime("%d%m%Y")}_{now.strftime("%H%M%S")}.txt', 'w',
-    #                   encoding='utf-8') as file:
-    #             file.write(f'Нам пришло сообщение от {now.strftime("%d.%m.%Y")} в {now.strftime("%H:%M:%S")}!\n'
-    #                        f'Отправитель: {email}\n'
-    #                        f'Тема: {title}\n'
-    #                        f'Текст: {text}')
-    #         return '200 OK', render('contact_lesson2.html', date=request.get('date', None))
-    #     else:
-    #         return '200 OK', render('contact_lesson2.html', date=request.get('date', None))
+        if request['method'] == 'POST':
+            # метод пост
+            data = request['data']
+
+            name = data['name']
+            name = site.decode_value(name)
+
+            category = None
+            if self.category_id != -1:
+                category = site.find_category_by_id(int(self.category_id))
+
+                course = site.create_course('record', name, category)
+                site.courses.append(course)
+
+            return '200 OK', render('course_list.html',
+                                    objects_list=category.courses,
+                                    name=category.name,
+                                    id=category.id)
+
+        else:
+            try:
+                self.category_id = int(request['request_params']['id'])
+                category = site.find_category_by_id(int(self.category_id))
+
+                return '200 OK', render('create_course.html',
+                                        name=category.name,
+                                        id=category.id)
+            except KeyError:
+                return '200 OK', 'No categories have been added yet'
 
 
+# контроллер - создать категорию
+class CreateCategory:
+    def __call__(self, request):
+
+        if request['method'] == 'POST':
+            # метод пост
+
+            data = request['data']
+
+            name = data['name']
+            name = site.decode_value(name)
+
+            category_id = data.get('category_id')
+
+            category = None
+            if category_id:
+                category = site.find_category_by_id(int(category_id))
+
+            new_category = site.create_category(name, category)
+
+            site.categories.append(new_category)
+
+            return '200 OK', render('index.html', objects_list=site.categories)
+        else:
+            categories = site.categories
+            return '200 OK', render('create_category.html',
+                                    categories=categories)
+
+
+# контроллер - список категорий
+class CategoryList:
+    def __call__(self, request):
+        logger.log('Список категорий')
+        return '200 OK', render('category_list.html',
+                                objects_list=site.categories)
+
+
+# контроллер - копировать курс
+class CopyCourse:
+    def __call__(self, request):
+        request_params = request['request_params']
+
+        try:
+            name = request_params['name']
+
+            old_course = site.get_course(name)
+            if old_course:
+                new_name = f'copy_{name}'
+                new_course = old_course.clone()
+                new_course.name = new_name
+                site.courses.append(new_course)
+
+            return '200 OK', render('course_list.html',
+                                    objects_list=site.courses,
+                                    name=new_course.category.name)
+        except KeyError:
+            return '200 OK', 'No courses have been added yet'
